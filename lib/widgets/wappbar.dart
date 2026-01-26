7d4f762714ce99804b56274f2599415ae73937b9
Adriell8000B <adrielluiz73@gmail.com>
2026-01-26 18:32:05 -0300

    Small changes

--- lib/widgets/wappbar.dart
+++ lib/widgets/wappbar.dart
@@ -6,13 +6,15 @@ class WAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: Text("Good Morning!"),
+      title: Text("Good Afternoon"),
       actions: [
         Padding(
           padding: EdgeInsets.only(right: 12, left: 12),
           child: CircleAvatar(
-            backgroundImage: NetworkImage("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%2Fid%2FOIP.AQcNdjwU5teB-FHbX4TBxwHaFj%3Fpid%3DApi&f=1&ipt=6fcd297adc2ca68b3e39c7dd87285bd145d69ef5223acc23c170a22a1d067b8e&ipo=images"),
-          )
+            backgroundImage: NetworkImage(
+              "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%2Fid%2FOIP.AQcNdjwU5teB-FHbX4TBxwHaFj%3Fpid%3DApi&f=1&ipt=6fcd297adc2ca68b3e39c7dd87285bd145d69ef5223acc23c170a22a1d067b8e&ipo=images",
+            ),
+          ),
         ),
       ],
     );
